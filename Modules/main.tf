8250a9224e0c57932cd1be2c721b70091d73406d
Afolabi Alabi <folabialabi@hotmail.com>
2025-01-18 13:31:45 -0500

    Initial commit: Add Terraform code for AWS S3 module

---
+++ Modules/main.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "bl-sample" {
+  bucket        = var.bucket_name
+   force_destroy = false
+
+  tags = {
+    Owner = "BL-Main"
+   
+  }
+}
+
+resource "aws_s3_bucket_policy" "bl-sample" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowReadAccess"
+        Effect    = "Allow"
+        Principal = ""
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.this.arn}/*"
+      }
+    ]
+  })
+}
---
+++ main.tf
@@ -0,0 +1,8 @@
+module "aws_s3_bucket" {
+  source       = "../Modules"
+  bucket_name  = var.bucket_name
+  force_destroy = false
+  tags = {
+    Owner = "BL-Main"
+  }
+}
