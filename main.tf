5c7abf415d3eb42be526af2edaa3ae3df40424f0
Flosuccess <105125919+Flosuccess@users.noreply.github.com>
2026-07-27 16:24:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 module "aws_s3_bucket" {
-  source       = "../Modules"
+  source       = "./Modules"
   bucket_name  = var.bucket_name
-  force_destroy = false
-  tags = {
-    Owner = "BL-K8S(kubernetes)"
-  }
+ 
 }
