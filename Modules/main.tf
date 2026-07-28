bd9568999fdf4e4b43c51efca3d26938fe080cfa
Flosuccess <105125919+Flosuccess@users.noreply.github.com>
2026-07-28 12:34:17 -0400

    Update main.tf

--- Modules/main.tf
+++ Modules/main.tf
@@ -1,19 +1,28 @@
-resource "aws_s3_bucket" "uptropical7" {
-  bucket        = var.bucket_name
+resource "aws_s3_bucket" "uptropical757" {
+  bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_policy" "uptropical7" {
-  bucket = aws_s3_bucket.uptropical7.id
+resource "aws_s3_bucket_policy" "uptropical757" {
+  bucket = aws_s3_bucket.uptropical757.id
 
   policy = jsonencode({
     Version = "2012-10-17"
+
     Statement = [
       {
-        Sid       = "AllowReadAccess"
-        Effect    = "Allow"
-        Principal = ""
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.uptropical7.arn}/*"
+        Sid    = "AllowTestUserAccess"
+        Effect = "Allow"
+
+        Principal = {
+          AWS = "arn:aws:iam::628234978954:user/test"
+        }
+
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+
+        Resource = "${aws_s3_bucket.uptropical757.arn}/*"
       }
     ]
   })
